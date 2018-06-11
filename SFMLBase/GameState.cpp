528be54246c47201e2c583d92dbbd8bafc6f5eb2
miguellopes91 <miguelpvz@hotmail.com>
2018-06-12 00:46:26 +0100

    Add game grid and pieces

--- SFMLBase/GameState.cpp
+++ SFMLBase/GameState.cpp
@@ -21,12 +21,27 @@ namespace Bardo {
 		turn = PLAYER_PIECE;
 
 		this->_data->assets.LoadTexture("Pause Button", PAUSE_BUTTON);
+		this->_data->assets.LoadTexture("Grid Sprite", GRID_SPRITE_FILEPATH);
+		this->_data->assets.LoadTexture("X Piece", X_PIECE_FILEPATH);
+		this->_data->assets.LoadTexture("Circle Piece", O_PIECE_FILEPATH);
+
 
 		_background.setTexture(this->_data->assets.GetTexture("Background"));
 		_pauseButton.setTexture(this->_data->assets.GetTexture("Pause Button"));
+		_gridSprite.setTexture(this->_data->assets.GetTexture("Pause Button"));
 
 		_pauseButton.setPosition(this->_data->window.getSize().x - _pauseButton.getLocalBounds().width,_pauseButton.getPosition().y);
+		_gridSprite.setPosition((SCREEN_WIDTH/2)- (_gridSprite.getGlobalBounds().width/2), (SCREEN_HEIGHT / 2) - (_gridSprite.getGlobalBounds().height / 2));
 
+		InitGridPieces();
+
+		for (int x = 0; x < 3; x++)
+		{
+			for (int y = 0; y < 3; y++)
+			{
+				gridArray[x][y] = EMPTY_PIECE;
+			}
+		}
 	}
 
 	void GameState::HandleInput()
@@ -58,9 +73,36 @@ namespace Bardo {
 
 		this->_data->window.draw(this->_background);
 		this->_data->window.draw(this->_pauseButton);
+		this->_data->window.draw(this->_gridSprite);
+
+		for (int x = 0; x < 3; x++)
+		{
+			for (int y = 0; y < 3; y++)
+			{
+				this->_data->window.draw(this->_gridPieces[x][y]);
+
+			}
+		}
 
 		this->_data->window.display();
 	}
+
+	void GameState::InitGridPieces()
+	{
+		sf::Vector2u tempSpriteSize = this->_data->assets.GetTexture("X Piece").getSize();
+
+		for (int x = 0; x < 3; x++)
+		{
+			for (int y = 0; y < 3; y++)
+			{
+				_gridPieces[x][y].setTexture(this->_data->assets.GetTexture("X Piece"));
+				_gridPieces[x][y].setPosition(_gridSprite.getPosition().x + (tempSpriteSize.x*x) - 7
+					, _gridSprite.getPosition().y + (tempSpriteSize.y*y) - 7);
+				_gridPieces[x][y].setColor(sf::Color(255, 255, 255, 255));
+			}
+		}
+	}
+
 
 }
 
--- SFMLBase/GameState.hpp
+++ SFMLBase/GameState.hpp
@@ -20,10 +20,17 @@ namespace Bardo {
 		void Draw(float dt);
 
 	private:
+		void InitGridPieces();
+
 		GameDataRef _data;
 
 		sf::Sprite _background;
 		sf::Sprite _pauseButton;
+		sf::Sprite _gridSprite;
+
+		sf::Sprite _gridPieces[3][3];
+
+		int gridArray[3][3];
 
 		int turn;
 		int gameState;
